d8d252df3a7cfa436e268d5f140e5e1619350273
Heather Bailey <heatherbailey0630@gmail.com>
2022-09-29 14:13:03 -0700

    Update Statistics

---
+++ MechaCarChallenge.R
@@ -0,0 +1,9 @@
+library(dplyr)
+MPG_Table <- read.csv('MechaCar_mpg.csv')
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=MPG_Table) #Generate Linear Regression
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=MPG_Table)) #Generate summary statistics
+Suspension_Table <- read.csv('Suspension_Coil.csv')
+total_summary <- Suspension_Table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #Create total summary table
+total_summary
+lot_summary <- Suspension_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #Create Lot Summary
+lot_summary
