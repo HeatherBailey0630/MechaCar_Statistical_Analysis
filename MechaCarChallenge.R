3d06cebd40026b227ba0b1ea63bf12e3642d8644
Heather Bailey <heatherbailey0630@gmail.com>
2022-09-29 14:39:37 -0700

    Final Update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,13 +1,34 @@
+#Add Library
 library(dplyr)
+
+#Import CSV
 MPG_Table <- read.csv('MechaCar_mpg.csv')
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=MPG_Table) #Generate Linear Regression
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=MPG_Table)) #Generate summary statistics
+
+#Generate Linear Regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=MPG_Table)
+
+#Generate summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=MPG_Table))
+
+#Import CSV
 Suspension_Table <- read.csv('Suspension_Coil.csv')
-total_summary <- Suspension_Table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #Create total summary table
+
+#Create total summary table
+total_summary <- Suspension_Table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
 total_summary
-lot_summary <- Suspension_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #Create Lot Summary
+
+#Create Lot Summary
+lot_summary <- Suspension_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
 lot_summary
+
+#Create Overall T_Test
 t.test(Suspension_Table$PSI,mu=mean(Suspension_Table$PSI))
+
+#Create T_Test for Lot1
 t.test(subset(Suspension_Table$PSI,Suspension_Table$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Table$PSI))
+
+#Create T_Test for Lot2
 t.test(subset(Suspension_Table$PSI,Suspension_Table$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Table$PSI))
+
+#Create T_Test for Lot3
 t.test(subset(Suspension_Table$PSI,Suspension_Table$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Table$PSI))
